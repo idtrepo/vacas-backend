7e75f4b5adb9ac2db8b891933adc11b9bcfea3c5
idtrepo <ifierro@idttecnologias.mx>
2024-03-27 09:14:47 -0700

    refactor(config/settings): llamando a las variables de entorno a traves de 'os.environ' en vez de 'dotenv'

--- config/settings.py
+++ config/settings.py
@@ -1,18 +1,13 @@
-from dotenv import load_dotenv, dotenv_values
+import os
 
 
-load_dotenv()
-env_data = {**dotenv_values('.env')}
-
-
-DB_USER = env_data.get('DB_USER')
-DB_PASS = env_data.get('DB_PASS')
-DB_HOST = env_data.get('DB_HOST')
-DB_PORT = env_data.get('DB_PORT')
-DB_NAME = env_data.get('DB_NAME')
-DB_DRIVER = env_data.get('DB_DRIVER')
-
-SECRET_KEY = env_data.get('SECRET_KEY')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+DB_DRIVER = os.environ.get('DB_DRIVER')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 def obtener_db_conexion():
