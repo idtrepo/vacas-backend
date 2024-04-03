a4edb3db6d0f72e1fae205ae86e5df1269639dc1
idtrepo <ifierro@idttecnologias.mx>
2024-04-03 14:43:48 -0700

    Cambio en la forma de leer variables de entorno

--- config/settings.py
+++ config/settings.py
@@ -1,13 +1,16 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_NAME = os.environ.get('DB_NAME')
-DB_DRIVER = os.environ.get('DB_DRIVER')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_DRIVER = os.getenv('DB_DRIVER')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 def obtener_db_conexion():
